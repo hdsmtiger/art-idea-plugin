a036d4e363a85dc956f59b55c4d2b9f1aa455ec7
GitHub Action <action@github.com>
2021-06-14 03:18:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/hdsmtiger/artideaplugin/MyBundle.kt
+++ src/main/kotlin/com/github/hdsmtiger/artideaplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hdsmtiger.artideaplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/hdsmtiger/artideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/hdsmtiger/artideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.hdsmtiger.artideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hdsmtiger.artideaplugin.MyBundle
 
 class MyApplicationService {
 
